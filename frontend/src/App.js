10ad41b73f7c73d25346db0d909f223a467447a4
Daniel Flores <daniel.flores4930@gmail.com>
2022-06-24 18:19:06 -0700

    added logout and profile buttons, with nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import * as sessionActions from './store/session';
 import LoginFormPage from './components/LoginFormPage';
 import SignupFormPage from './components/SignUpFormPage';
+import Navigation from './components/Navigation';
 
 function App() {
   const dispatch = useDispatch();
@@ -14,14 +15,17 @@ function App() {
   }, [dispatch]);
 
   return isLoaded && (
-    <Switch>
-      <Route path="/login">
-        <LoginFormPage />
-      </Route>
-      <Route path="/signup">
-        <SignupFormPage />
-      </Route>
-    </Switch>
+    <>
+    <Navigation isLoaded={isLoaded} />
+      <Switch>
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+        <Route path="/signup">
+          <SignupFormPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
