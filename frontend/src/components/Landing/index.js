a3298dbfa1d651766bbb85d49db7bcfcc200abc6
Daniel Flores <daniel.flores4930@gmail.com>
2022-07-26 22:39:34 -0700

    added gray background to site

--- frontend/src/components/Landing/index.js
+++ frontend/src/components/Landing/index.js
@@ -16,7 +16,7 @@ export default function LandingPage(props) {
     }, [dispatch])
 
     return (
-        <div style={{display: "flex", alignItems: "center", flexDirection: "column", color: "white"}}>
+        <div style={{display: "flex", alignItems: "center", flexDirection: "column", color: "white", backgroundColor: "white"}}>
             <img src="https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80" alt="banner" style={{width: "90vw", height: "70vh", objectFit: "cover", borderTop: "solid 5px orange"}} />
             <div style={{position: "absolute", top: "15rem", textAlign: "center"}}>
                 <h2>Start Exploring Today!</h2>
