5d8572cf361b44ef59c5e25aef495d396778ba7e
Daniel Flores <daniel.flores4930@gmail.com>
2022-10-03 23:40:46 -0700

    more styling and functionality to match target site

--- frontend/src/components/UploadSong/UploadSongForm.js
+++ frontend/src/components/UploadSong/UploadSongForm.js
@@ -58,15 +58,23 @@ export default function UploadSongForm({ songFiles, initialTitle }) {
         if (file) setImage(file);
     };
 
+    const handleClick = e => {
+        e.preventDefault();
+        const fakeBtn = document.getElementById('fake-img-upld')
+        fakeBtn.click();
+    }
+
     console.log("this is the image => ", image)
 
     return (
-        <div className='flx-ctr'>
+        <form className='flx-ctr form-container'>
             {hasSubmitted && !!errors.length && errors.map(error => <div key={error}>{error}</div>)}
-            <div className="left-side">
+            <div className="left-side flx-ctr">
                 <img src={image ? window.URL.createObjectURL(image) : 'https://qph.cf2.quoracdn.net/main-qimg-0b4d3539b314fb898a95d424fe1af853-pjlq'} alt="song-cover" className="input-image" />
+                <input type="file" accept="image/jpeg, image/png" onChange={updateImageFile} id="fake-img-upld" hidden />
+                <button className="sng-img-upld-btn flx-ctr" onClick={handleClick}><span className="material-symbols-outlined">add_a_photo</span>Upload Image</button>
             </div>
-            <form
+            <div
                 className="right-side flx-col"
                 onSubmit={handleSubmit}
             >
@@ -88,11 +96,9 @@ export default function UploadSongForm({ songFiles, initialTitle }) {
                     onChange={(e) => setDescription(e.target.value)}
                     required
                 />
-                <label>Choose an image for this song</label>
-                <input type="file" accept="image/jpeg, image/png" onChange={updateImageFile} />
                 <button className="org-btn flx-ctr" type="submit">Upload Song</button>
-            </form>
-        </div>
+            </div>
+        </form>
 
     )
 }
