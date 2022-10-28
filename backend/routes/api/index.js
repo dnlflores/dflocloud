19b8085a77688d8d1f3fa16b9f446151ff27b502
Daniel Flores <daniel.flores4930@gmail.com>
2022-10-28 14:27:58 -0700

    basic search function complete still need results page

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
-// const asyncHandler = require('express-async-handler');
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
+const asyncHandler = require('express-async-handler');
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const songsRouter = require('./songs.js');
@@ -9,6 +7,8 @@ const commentsRouter = require('./comments.js');
 const albumsRouter = require('./albums.js');
 const playlistsRouter = require('./playlists.js')
 const { restoreUser } = require('../../utils/auth.js');
+const { Song, Playlist } = require('../../db/models');
+const { Op } = require('sequelize');
 
 router.use(restoreUser);
 router.use('/session', sessionRouter);
@@ -24,39 +24,80 @@ router.get('/csrf/restore', function (req, res) {
     res.send({ 'XSRF-TOKEN': req.csrfToken() });
 });
 
-// router.post('/test', (req, res) => {
-//   res.json({ requestBody: req.body });
-// });
+router.get('/search', asyncHandler(async (req, res) => {
+    const { term } = req.query;
 
-// // GET /api/set-token-cookie
-// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
-//     const user = await User.findOne({
-//         where: {
-//             username: 'Demo-lition'
-//         }
-//     });
-//     setTokenCookie(res, user);
-//     return res.json({ user });
-// }));
+    const matchedSongs = process.env.NODE_ENV === 'production' ?
+    
+    await Song.findAll({
+        where: {
+            [Op.or]: [
+                {
+                    title: {
+                        [Op.iLike]: `%${term}%`
+                    }
+                },
+                {
+                    description: {
+                        [Op.iLike]: `%${term}%`
+                    }
+                }
+            ]
+        }
+    }) :
+    await Song.findAll({
+        where: {
+            [Op.or]: [
+                {
+                    title: {
+                        [Op.substring]: term
+                    }
+                },
+                {
+                    description: {
+                        [Op.substring]: term
+                    }
+                }
+            ]
+        }
+    });
 
-// // GET /api/restore-user
-// const { restoreUser } = require('../../utils/auth.js');
-// router.get(
-//   '/restore-user',
-//   restoreUser,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
+    const matchedPlaylists = process.env.NODE_ENV === 'production' ?
+    
+    await Playlist.findAll({
+        where: {
+            [Op.or]: [
+                {
+                    name: {
+                        [Op.iLike]: `%${term}%`
+                    }
+                },
+                {
+                    description: {
+                        [Op.iLike]: `%${term}%`
+                    }
+                }
+            ]
+        }
+    }) :
+    await Playlist.findAll({
+        where: {
+            [Op.or]: [
+                {
+                    name: {
+                        [Op.substring]: term
+                    }
+                },
+                {
+                    description: {
+                        [Op.substring]: term
+                    }
+                }
+            ]
+        }
+    });
 
-// // GET /api/require-auth
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
+    return res.json({ songs: matchedSongs, playlists: matchedPlaylists });
+}))
 
 module.exports = router;
--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -1,17 +1,56 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import useDebounce from '../../hooks/useDebounce';
 import './SearchBar.css';
 
 export default function SearchBar() {
+    const history = useHistory();
     const [search, setSearch] = useState('');
+    const [results, setResults] = useState({});
+    const [showResults, setShowResults] = useState(false);
 
-    console.log("here is the search term => ", search);
-    useDebounce(() => console.log("fetch api with search term: ", search), 1000, [search]);
+    useDebounce(() => {
+        if (search !== '') fetch(`/api/search?term=${search}`).then(res => res.json().then(data => setResults(data)));
+        else if (search === '') setResults({})
+    }, 500, [search]);
+
+    useEffect(() => {
+        if (!search) setShowResults(false)
+        else setShowResults(true)
+    }, [results]);
+
+    const goToResult = (typeResult, resultId) => {
+        history.push(`/${typeResult}s/${resultId}`);
+        setShowResults(false);
+        setSearch('');
+        setResults({});
+    };
 
     return (
-        <div className="flx-ctr search-bar">
-            <input type="text" onChange={e => setSearch(e.target.value)} value={search} placeholder="Search" />
-            <button><span className="material-symbols-outlined">search</span></button>
+        <div className="flx-ctr flx-col search-wrapper">
+            <div className="flx-ctr search-bar">
+                <input type="text" onChange={e => setSearch(e.target.value)} value={search} placeholder="Search" onFocus={() => search ? setShowResults(true) : setShowResults(false)} />
+                <button><span className="material-symbols-outlined">search</span></button>
+            </div>
+            {showResults && (
+                <>
+                    <div className="closable" onClick={() => setShowResults(false)} />
+                    <div style={{ position: 'relative' }}>
+                        <div className="flx-ctr flx-col search-results">
+                            {Object.values(results.songs).map(song => (
+                                <div key={song.id} className="flx-ctr" onClick={() => goToResult('song', song.id)}>
+                                    <h3><span className="material-symbols-outlined">music_note</span>&nbsp;{song.title}</h3>
+                                </div>
+                            ))}
+                            {Object.values(results.playlists).map(playlist => (
+                                <div key={playlist.id} className="flx-ctr" onClick={() => goToResult('playlist', playlist.id)}>
+                                    <h3><span className="material-symbols-outlined">queue_music</span>&nbsp;{playlist.name}</h3>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
