1d0b1a8c47429cc558530da177e754820105f17b
Daniel Flores <daniel.flores4930@gmail.com>
2022-11-01 13:58:39 -0700

    messing with seed data

--- backend/db/seeders/20220709031855-addSongsToPlaylists.js
+++ backend/db/seeders/20220709031855-addSongsToPlaylists.js
@@ -1,13 +1,14 @@
 'use strict';
-const playlistSongs = [...Array(500)].map((playlist, idx) => (
-  {
-    songId: Math.floor(Math.random() * 30) + 1,
-    playlistId: Math.floor(Math.random() * 50) + 1,
+const playlistSongs = [...Array(500)].map((playlist, idx) => {
+
+  return {
+    songId: (idx % 30) + 1,
+    playlistId: (idx % 50) + 1,
     index: idx % 10,
     createdAt: new Date(),
     updatedAt: new Date()
   }
-));
+});
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
