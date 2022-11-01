ff164e2b93384092ad85027bd3340c1cc5608bbd
Daniel Flores <daniel.flores4930@gmail.com>
2022-11-01 14:31:16 -0700

    seeding

--- backend/db/seeders/20220709031855-addSongsToPlaylists.js
+++ backend/db/seeders/20220709031855-addSongsToPlaylists.js
@@ -1,9 +1,22 @@
 'use strict';
+const songIDs = [];
+const playlistIDs = [];
+
+while(songIDs.length < 30){
+    const r = Math.floor(Math.random() * 30) + 1;
+    if(songIDs.indexOf(r) === -1) songIDs.push(r);
+}
+
+while(playlistIDs.length < 50){
+  const r = Math.floor(Math.random() * 50) + 1;
+  if(playlistIDs.indexOf(r) === -1) playlistIDs.push(r);
+}
+
 const playlistSongs = [...Array(500)].map((playlist, idx) => {
 
   return {
-    songId: (idx % 30) + 1,
-    playlistId: (idx % 50) + 1,
+    songId: songIDs[idx % 30],
+    playlistId: playlistIDs[idx % 50],
     index: idx % 10,
     createdAt: new Date(),
     updatedAt: new Date()
