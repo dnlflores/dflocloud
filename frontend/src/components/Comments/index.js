0da6a49bed8dbd405ca4643e4dbe348f9009549f
Daniel Flores <daniel.flores4930@gmail.com>
2022-11-30 11:12:24 -0800

    playing with some APIs

--- frontend/src/components/Comments/index.js
+++ frontend/src/components/Comments/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getComments, removeComment } from '../../store/comments';
@@ -9,21 +9,32 @@ export default function Comments({ song }) {
     const commentsObj = useSelector(state => state.comments);
     const currentUser = useSelector(state => state.session.user);
     const comments = Object.values(commentsObj || {});
+    const [dummyComments, setDummyComments] = useState([]);
 
     useEffect(() => {
         dispatch(getComments(songId));
+        getDummyComments(songId);
     }, [dispatch, songId]);
 
     const handleDelete = async event => {
         await dispatch(removeComment(event.target.id));
     };
 
+    const getDummyComments = async page => {
+        const response = await fetch(`https://dummyapi.io/data/v1/comment?page=${page}`, { headers: { 'app-id': '6386edf58be5d48814c77783' } });
+        const fakeComments = await response.json();
+        console.log("dummy comments function running", fakeComments);
+        setDummyComments(fakeComments.data);
+    };
+
+    console.log("here are the dummy comments => ", dummyComments);
+
     return (
         <div className="flx-col comment-list">
             <div className="page-description">
                 <p style={{ color: 'black' }}>{song.description}</p>
             </div>
-            {!!comments.length && <p><span className="material-symbols-outlined">chat_bubble</span>{`${comments.length} Comments`}</p>}
+            <p><span className="material-symbols-outlined">chat_bubble</span>{`${comments.length + dummyComments.length} Comments`}</p>
             {!!comments.length && comments.map(comment => (
                 <div className="comment-card" key={comment.id}>
                     <div className="comment-info">
@@ -36,6 +47,17 @@ export default function Comments({ song }) {
                     {currentUser && +currentUser.id === +comment.userId && (
                         <button className="flx-ctr delete-comment" onClick={handleDelete} value={comment.id}><span className="material-symbols-outlined" id={comment.id}>delete</span></button>
                     )}
+                </div>
+            ))}
+            {!!dummyComments.length && dummyComments.map(comment => (
+                <div className="comment-card" key={comment.id}>
+                    <div className="comment-info">
+                        <img src={comment.owner.picture} alt="comment-user" />
+                        <div className="user-info">
+                            <p>{comment.owner.firstName} {comment.owner.lastName}</p>
+                            <p style={{ color: 'black' }}>{comment.message}</p>
+                        </div>
+                    </div>
                 </div>
             ))}
         </div>
